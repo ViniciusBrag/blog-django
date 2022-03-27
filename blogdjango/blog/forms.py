cda5f6f35f98f197da8341a9e2b9f9695d7e6d04
Vinicius Braga <Vbragadev@gmail.com>
2022-03-27 20:27:30 -0300

    add form in project

---
+++ blogdjango/blog/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from .models import Post
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'slug', 'author', 'content', 'status_post')
+
+        widget = {
+            'title': forms.TextInput(attrs={'class': 'form-control',}),
+            'slug': forms.TextInput(attrs={'class': 'form-control'}),
+            'author': forms.Select(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control'}),
+        }
+
+
