38b1e468d33185a63bdd6505df867fc9d873e69c
Vinicius Braga <Vbragadev@gmail.com>
2022-03-27 20:45:12 -0300

    test

--- blogdjango/blog/tests/test_post.py
+++ blogdjango/blog/tests/test_post.py
@@ -1,10 +1,11 @@
 
 from django.urls.base import reverse
 import pytest
+from blogdjango import blog
 
 @pytest.fixture
 def resp(client):
-    resp = client.get(reverse('blog:base'))
+    resp = client.get(reverse('blog:home'))
     return resp
 
 def test_status_code(resp):
