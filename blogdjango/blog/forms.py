c99f9b380525707ec10f5027ef3e147a4c9f38db
Vinicius Braga <Vbragadev@gmail.com>
2022-03-28 02:30:20 -0300

    add edit and update post

--- blogdjango/blog/forms.py
+++ blogdjango/blog/forms.py
@@ -1,2 +1,29 @@
+from django import forms
+from .models import Post
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'slug', 'status_post', 'author', 'content')
+
+        widget = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'slug': forms.TextInput(attrs={'class': 'form-control'}),
+            'author': forms.Select(attrs={'class': 'form-control'}),
+            'status_post': forms.Select(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control'}),
+        }    
 
 
+class EditForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'slug', 'status_post', 'author', 'content')
+
+        widget = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'slug': forms.TextInput(attrs={'class': 'form-control'}),
+            'author': forms.Select(attrs={'class': 'form-control'}),
+            'status_post': forms.Select(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control'}),
+        }    
--- blogdjango/blog/urls.py
+++ blogdjango/blog/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path("", views.PostList.as_view(), name="home"),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('/about_blog/', views.AboutBlog.as_view(), name='about_blog'),
+    path('add_post', views.AddPostView.as_view(), name='add_post'),
+    path('/edit/<int:pk>', views.UpdatePostView.as_view(), name='update_post'),
 ]
