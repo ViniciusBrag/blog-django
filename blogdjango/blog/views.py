fd9687046d11fd5d85ee0bbf9198b3050fec6fda
Vinicius Braga <Vbragadev@gmail.com>
2022-03-28 02:30:20 -0300

    add edit and update post

--- blogdjango/blog/views.py
+++ blogdjango/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from blogdjango.blog.forms import EditForm, PostForm
 from blogdjango.blog.models import Post, AboutBlog 
-from django.views.generic import DetailView, ListView
+from django.views.generic import DetailView, ListView, CreateView, UpdateView
 
 class PostList(ListView):
     queryset = Post.objects.filter(status_post=1).order_by('-created_on')
@@ -16,3 +17,15 @@ class AboutBlog(ListView):
     model = AboutBlog
     template_name = 'about_blog.html'
         
+
+class AddPostView(CreateView):
+    model = Post
+    form_class = EditForm
+    template_name = 'add_post.html' 
+    
+
+class UpdatePostView(UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'update_post.html'
+   
